544f0fc7abb8b314e177fa0a2b3454ad4d4720f9
ahmed <aaboelwafa@effvision.com>
2024-05-16 18:04:07 +0300

    update key

--- components/render-components/grid-cards/card.jsx
+++ components/render-components/grid-cards/card.jsx
@@ -1,32 +1,31 @@
-
-
 import Image from 'next/image';
 import { IMAGE_URL } from '../../../lib/constants';
 import styles from './grid-card.module.css';
 export const Card = ({ gridCard }) => {
-    const imgSrc = gridCard?.field_card_image?.field_media_image?.uri?.url || '';
-    return (
-        <div className={styles["card-with-image"]}>
-            <Image
-                src={IMAGE_URL + imgSrc}
-                width="526"
-                height="400"
-                alt="image page"
-            />
-            <h4 className={`medium-1`}>{gridCard.field_card_title}</h4>
-            <p
-                dangerouslySetInnerHTML={{
-                    __html: gridCard.field_card_description.value,
-                }}>
-            </p>
-            <div className={styles["link-wrapper"]}>
-                {gridCard.field_card_link.map((link) => (
-                    <a href={link.uri.replace("internal:","")}>{link.title}</a>
-                ))}
-
-            </div>
-        </div>
-    );
+	const imgSrc = gridCard?.field_card_image?.field_media_image?.uri?.url || '';
+	return (
+		<div className={styles['card-with-image']}>
+			<Image
+				src={IMAGE_URL + imgSrc}
+				width="526"
+				height="400"
+				alt="image page"
+			/>
+			<h4 className={`medium-1`}>{gridCard.field_card_title}</h4>
+			<p
+				dangerouslySetInnerHTML={{
+					__html: gridCard.field_card_description.value,
+				}}
+			></p>
+			<div className={styles['link-wrapper']}>
+				{gridCard.field_card_link.map((link) => (
+					<a key={link.id} href={link.uri.replace('internal:', '')}>
+						{link.title}
+					</a>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default Card;
